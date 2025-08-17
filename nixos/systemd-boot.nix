6634304fc1572496a839c50cc8a258970024c6d2
hcw9iu <hcw.chiu@gmail.com>
2025-08-17 12:37:34 +0800

    6.12

--- nixos/systemd-boot.nix
+++ nixos/systemd-boot.nix
@@ -11,7 +11,7 @@
     };
     tmp.cleanOnBoot = true;
     kernelPackages =
-      pkgs.linuxPackages_6_15; # _zen, _hardened, _rt, _rt_latest, etc.
+      pkgs.linuxPackages_6_12; # _zen, _hardened, _rt, _rt_latest, etc.
       #pkgs.linuxPackages_latest;
 
     # Silent boot
