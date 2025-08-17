3936c468850a9507ef7a83e7ba81b1a8e887fa9b
hcw9iu <hcw.chiu@gmail.com>
2025-08-17 13:14:31 +0800

    560

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -5,7 +5,7 @@ let
   unstablePkgs = import inputs."nvidia-src" { system = pkgs.system; };
 
   # 560.x 版驅動在 unstable 的 derivation
-  upstreamDrv = unstablePkgs.linuxPackages_latest.nvidia_x11;
+  upstreamDrv = unstablePkgs.linuxPackages_latest.nvidia_x11_560;
 
   # 以目前 kernelPackages 為基礎，重新 build 與核心相容的驅動
   nvidiaDriverChannel = config.boot.kernelPackages.nvidiaPackages.mkDriver {
