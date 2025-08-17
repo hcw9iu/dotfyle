64a5ed2ade649351c9b166541c6c377cbdbea616
hcw9iu <hcw.chiu@gmail.com>
2025-08-17 09:30:26 +0800

    sddm update to minimal

--- nixos/sddm.nix
+++ nixos/sddm.nix
@@ -1,61 +1,14 @@
-# SDDM is a display manager for X11 and Wayland
 {
   pkgs,
-  inputs,
-  config,
   ...
-}: let
-  foreground = config.theme.textColorOnWallpaper;
-  #sddm-astronaut = pkgs.sddm-astronaut.override {
-  sddmAstronaut = pkgs.sddm-astronaut-theme.override {
-    embeddedTheme = "pixel_sakura";
-    themeConfig = {
-      HeaderTextColor = "#${foreground}";
-      DateTextColor = "#${foreground}";
-      TimeTextColor = "#${foreground}";
-      LoginFieldTextColor = "#${foreground}";
-      PasswordFieldTextColor = "#${foreground}";
-      UserIconColor = "#${foreground}";
-      PasswordIconColor = "#${foreground}";
-      WarningColor = "#${foreground}";
-      LoginButtonBackgroundColor = "#${foreground}";
-      SystemButtonsIconsColor = "#${foreground}";
-      SessionButtonTextColor = "#${foreground}";
-      VirtualKeyboardButtonTextColor = "#${foreground}";
-      DropdownBackgroundColor = "#${foreground}";
-      HighlightBackgroundColor = "#${foreground}";
-      Background =
-        if "sakura_pixelart_light_static.png" == config.stylix.image
-        then
-          pkgs.fetchurl {
-            url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/sakura_pixelart_light_animated.gif";
-            sha256 = "sha256-qySDskjmFYt+ncslpbz0BfXiWm4hmFf5GPWF2NlTVB8=";
-          }
-        else if "cat-watching-the-star_pixelart_purple_static.png" == config.stylix.image
-        then
-          pkgs.fetchurl {
-            url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/cat-watching-the-star_pixelart_purple_animated.gif";
-            sha256 = "";
-          }
-        else "${toString config.stylix.image}";
-    };
-  };
-in {
-  services.displayManager = {
-    sddm = {
-      package = pkgs.kdePackages.sddm;
-      extraPackages = [sddmAstronaut];
-      enable = true;
-      wayland.enable = true;
-      #theme = "sddm-astronaut-theme";
-      theme = "sddm-astronaut";
-      settings = {
-        Wayland.SessionDir = "${
-          inputs.hyprland.packages."${pkgs.system}".hyprland
-        }/share/wayland-sessions";
-      };
-    };
+}: {
+  services.displayManager.sddm = {
+    enable         = true;          # 開啟 SDDM
+    wayland.enable = true;          # 同時支援 Wayland
+    theme          = "sddm-sugar-dark";
   };
 
-  environment.systemPackages = [sddmAstronaut];
+  environment.systemPackages = with pkgs; [
+    sddm-sugar-dark             # 安裝主題檔
+  ];
 }
