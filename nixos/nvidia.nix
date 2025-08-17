ba9f1fee5a00b361bd8089bf6cce46873b1e02df
hcw9iu <hcw.chiu@gmail.com>
2025-08-17 10:47:39 +0800

    update driver

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -1,53 +1,72 @@
-{ pkgs, config, ... }:
-
+{ lib, pkgs, config, ... }:
 let
-  nvidiaDriver = config.boot.kernelPackages.nvidiaPackages.beta;
-in {
-  # 只載入 nvidia driver
-  services.xserver.videoDrivers = [ "nvidia" ];
+  nvidiaDriverChannel =
+    config.boot.kernelPackages.nvidiaPackages.latest; # stable, latest, beta, production, etc.
+    #config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #  version = "565.57.01";
+    #  sha256_64bit  = "0yic33xx1b3jbgciphlwh6zqfj21vx9439zm0j45wf2yb17fksvf";
+    #  settingsSha256 = "1v8z8c895gvvr2y3974iahjpll8wjimv6w4g4qc9h460qhwc3k2b";
+    #  persistencedSha256 = "1qpsrmxz3y741qh4x91qbrkkc0x8hnifp7pfrsxmwnwf796r2904";
 
-  boot = {
-    kernelParams = [
+    #};
+in {
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers =
+    [ "nvidia" "displayLink" ]; # or "nvidiaLegacy470 etc.
+  boot.kernelParams =
+    lib.optionals (lib.elem "nvidia" config.services.xserver.videoDrivers) [
       "nvidia-drm.modeset=1"
+      "nvidia_drm.fbdev=1"
       "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
     ];
-    # 若想徹底不讓 iGPU 驅動載入，可一起封鎖
-    blacklistedKernelModules = [ "nouveau" "amdgpu" "radeon" ];
-  };
-
   environment.variables = {
-    GBM_BACKEND               = "nvidia-drm";
+    # GBM_BACKEND = "nvidia-drm"; # If crash in firefox, remove this line
+    LIBVA_DRIVER_NAME = "nvidia"; # hardware acceleration
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    LIBVA_DRIVER_NAME         = "nvidia";
-    NIXOS_OZONE_WL            = "1";
-    MOZ_ENABLE_WAYLAND        = "1";
+    NVD_BACKEND = "direct";
   };
-
   nixpkgs.config = {
     nvidia.acceptLicense = true;
-    allowUnfree = true;
+    allowUnfreePredicate = pkg:
+      builtins.elem (lib.getName pkg) [
+        "cudatoolkit"
+        "nvidia-persistenced"
+        "nvidia-settings"
+        "nvidia-x11"
+      ];
   };
-
   hardware = {
     nvidia = {
-      open                 = false;
-      nvidiaSettings        = true;
-      powerManagement.enable = true;
-      modesetting.enable     = true;
-      package                = nvidiaDriver;
-      # 移除 prime 區段 → 完全不啟用 iGPU
+      open = false;
+      nvidiaSettings = true;
+      powerManagement.enable =
+        true; # This can cause sleep/suspend to fail and saves entire VRAM to /tmp/
+      modesetting.enable = true;
+      package = nvidiaDriverChannel;
     };
-
+    #graphics = {
+      #enable = true;
+      #package = nvidiaDriverChannel;
+      #enable32Bit = true;
+      #extraPackages = with pkgs; [
+        #nvidia-vaapi-driver
+        #vaapiVdpau
+        #libvdpau-va-gl
+        #mesa
+        #egl-wayland
+      #];
+    #};
     opengl = {
-      enable          = true;
+      enable = true;
       driSupport32Bit = true;
-      package         = nvidiaDriver;
-      extraPackages   = with pkgs; [ egl-wayland ];
+      package = nvidiaDriverChannel;
+      extraPackages = with pkgs; [
+        nvidia-vaapi-driver
+        vaapiVdpau
+        libvdpau-va-gl
+        mesa
+        egl-wayland
+      ];
     };
   };
-
-  environment.systemPackages = with pkgs; [
-    vulkan-tools
-    glxinfo
-  ];
 }
