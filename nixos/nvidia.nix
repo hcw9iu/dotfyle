ccf0b31595a97fc49283fa31caf1a89eb22f1322
hcw9iu <hcw.chiu@gmail.com>
2025-08-17 16:21:42 +0800

    test

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -5,8 +5,8 @@ let
   unstablePkgs = import inputs."nvidia-src" { system = pkgs.system; };
 
   # 560.x 版驅動在 unstable 的 derivation
-  #upstreamDrv = unstablePkgs.linuxPackages_6_12.nvidia_x11;
-  upstreamDrv = unstablePkgs.linuxPackages_latest.nvidiaPackages.stable;
+  upstreamDrv = unstablePkgs.linuxPackages_6_12.nvidia_x11;
+  #upstreamDrv = unstablePkgs.linuxPackages_latest.nvidiaPackages.stable;
 
 
 
