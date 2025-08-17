7804f27950e46f13350ddb3e39f5094c46f6cf20
hcw9iu <hcw.chiu@gmail.com>
2025-08-17 11:28:39 +0800

    sha

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -1,13 +1,16 @@
 { lib, pkgs, config, ... }:
 let
   nvidiaDriverChannel =
-    config.boot.kernelPackages.nvidiaPackages.latest; # stable, latest, beta, production, etc.
-    #config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #config.boot.kernelPackages.nvidiaPackages.stable; # stable, latest, beta, production, etc.
+    config.boot.kernelPackages.nvidiaPackages.mkDriver {
       #version = "565.57.01";
       #sha256_64bit  = "0yic33xx1b3jbgciphlwh6zqfj21vx9439zm0j45wf2yb17fksvf";
       #settingsSha256 = "1v8z8c895gvvr2y3974iahjpll8wjimv6w4g4qc9h460qhwc3k2b";
       #persistencedSha256 = "1qpsrmxz3y741qh4x91qbrkkc0x8hnifp7pfrsxmwnwf796r2904";
-    #};
+      sha256 = lib.fakeSha256;
+      settingsSha256 = lib.fakeSha256;
+      persistencedSha256 = lib.fakeSha256;
+    };
     #nvidiaDriverChannel = pkgs.linuxPackages_latest.nvidiaPackages.latest;
 in {
   # Load nvidia driver for Xorg and Wayland
