cb0cb257fa99bbbf61e117d7c0aac95aa7a74e25
hcw9iu <hcw.chiu@gmail.com>
2025-08-17 11:33:53 +0800

    sha

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -7,7 +7,7 @@ let
       #sha256_64bit  = "0yic33xx1b3jbgciphlwh6zqfj21vx9439zm0j45wf2yb17fksvf";
       #settingsSha256 = "1v8z8c895gvvr2y3974iahjpll8wjimv6w4g4qc9h460qhwc3k2b";
       #persistencedSha256 = "1qpsrmxz3y741qh4x91qbrkkc0x8hnifp7pfrsxmwnwf796r2904";
-      sha256_64bit       = "0000000000000000000000000000000000000000000000000000";
+      sha256_64bit       = "H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTz1Qa=";
       settingsSha256     = "0000000000000000000000000000000000000000000000000000";
       persistencedSha256 = "0000000000000000000000000000000000000000000000000000";
     };
