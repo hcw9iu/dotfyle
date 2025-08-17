7e8da1d0dc4bee10626df42432712d457443f2a8
hcw9iu <hcw.chiu@gmail.com>
2025-08-17 16:26:44 +0800

    test

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -1,44 +1,35 @@
-{ lib, pkgs, config, inputs, ... }:
-
+{ lib, pkgs, config, ... }:
 let
-  # 先匯入 nixos-unstable 的 pkgs，只拿來取得最新版 NVIDIA 原始碼
-  unstablePkgs = import inputs."nvidia-src" { system = pkgs.system; };
-
-  # 560.x 版驅動在 unstable 的 derivation
-  upstreamDrv = unstablePkgs.linuxPackages_6_12.nvidia_x11;
-  #upstreamDrv = unstablePkgs.linuxPackages_latest.nvidiaPackages.stable;
-
-
-
-  # 以目前 kernelPackages 為基礎，重新 build 與核心相容的驅動
-  nvidiaDriverChannel = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version            = upstreamDrv.version;                  # 例：560.42.04
-    sha256_64bit       = upstreamDrv.src.outputHash;
-
-    #settingsSha256     = upstreamDrv.settings.src.outputHash;
-    persistencedSha256 = upstreamDrv.persistenced.src.outputHash;
-    #sha256_32bit        = null;
-  };
+  nvidiaDriverChannel =
+    config.boot.kernelPackages.nvidiaPackages.latest; # stable, latest, beta, production, etc.
+    #config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      #version = "565.77"; #stable
+      #sha256_64bit       = "0z0lncf3q4ndf16k928vpjrzvc9xgg8h494qcvbk9kvbqi1afyha";
+      #sha256_32bit       = "0z0lncf3q4ndf16k928vpjrzvc9xgg8h494qcvbk9kvbqi1afyha";
+      ##settingsSha256     = "0jds62i0pymn1riklkfdhq1jwzip0brhv0qz5kzjqfg5fa7ssism";
+      #persistencedSha256 = "031b583hndq5c9c93j6py6yzxhkf08yz9ac16iywf3vx9w5y6w62";
+    #};
+    #nvidiaDriverChannel = pkgs.linuxPackages_latest.nvidiaPackages.latest;
 in {
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" "displayLink" ]; # or "nvidiaLegacy470 etc.
-
-  boot.kernelParams = lib.optionals (lib.elem "nvidia" config.services.xserver.videoDrivers) [
-    "nvidia-drm.modeset=1"
-    "nvidia_drm.fbdev=1"
-    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-  ];
-
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
+   
+  services.xserver.videoDrivers =
+    [ "nvidia" "displayLink" ]; # or "nvidiaLegacy470 etc.
+  boot.kernelParams =
+    lib.optionals (lib.elem "nvidia" config.services.xserver.videoDrivers) [
+      "nvidia-drm.modeset=1"
+      "nvidia_drm.fbdev=1"
+      "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    ];
   environment.variables = {
     # GBM_BACKEND = "nvidia-drm"; # If crash in firefox, remove this line
-    LIBVA_DRIVER_NAME         = "nvidia"; # hardware acceleration
+    LIBVA_DRIVER_NAME = "nvidia"; # hardware acceleration
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    NVD_BACKEND               = "direct";
+    NVD_BACKEND = "direct";
   };
-
   nixpkgs.config = {
-    allowUnfree = true;
-    nvidia.acceptLicense   = true;
+    nvidia.acceptLicense = true;
     allowUnfreePredicate = pkg:
       builtins.elem (lib.getName pkg) [
         "cudatoolkit"
@@ -47,22 +38,32 @@ in {
         "nvidia-x11"
       ];
   };
-
   hardware = {
     nvidia = {
-      open                   = true;
-      nvidiaSettings         = false;
-      powerManagement.enable = true;  # May affect sleep/suspend
-      modesetting.enable     = true;
-      #package                = nvidiaDriverChannel;
-      package                = upstreamDrv;
+      open = open;
+      nvidiaSettings = false;
+      powerManagement.enable =
+        true; # This can cause sleep/suspend to fail and saves entire VRAM to /tmp/
+      modesetting.enable = true;
+      package = nvidiaDriverChannel;
     };
-
+    #graphics = {
+      #enable = true;
+      #package = nvidiaDriverChannel;
+      #enable32Bit = true;
+      #extraPackages = with pkgs; [
+        #nvidia-vaapi-driver
+        #vaapiVdpau
+        #libvdpau-va-gl
+        #mesa
+        #egl-wayland
+      #];
+    #};
     opengl = {
-      enable          = true;
+      enable = true;
       driSupport32Bit = true;
-      package         = nvidiaDriverChannel;
-      extraPackages   = with pkgs; [
+      package = nvidiaDriverChannel;
+      extraPackages = with pkgs; [
         nvidia-vaapi-driver
         vaapiVdpau
         libvdpau-va-gl
@@ -71,7 +72,4 @@ in {
       ];
     };
   };
-  #environment.systemPackages = [
-    #(import inputs."nvidia-src" { system = pkgs.system; }).nvidia-settings
-  #];
 }
