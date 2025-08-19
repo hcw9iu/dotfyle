5fa9eb799b756fd03ae4e44696916497d75ab439
hcw9iu <hcw.chiu@gmail.com>
2025-08-19 11:41:04 +0800

    test

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   inputs = {
     #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     #nixpkgs-24_05.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11/b134951a4c9f3c995fd7be05f3243f8ecd65d798"; 
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11/61b238b594341af3d5f38bfd419d0ae37e700f68"; 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master"; 
     nur.url = "github:nix-community/NUR";
     home-manager = {
